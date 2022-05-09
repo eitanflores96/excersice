7f2b59d8a769ddb7d9f3af9cf491b950dc5f3fef
eitanflores96 <75388083+eitanflores96@users.noreply.github.com>
2022-05-09 00:02:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-import Got from 'got'
+import weather from './routes/weather.js'
 import Fastify from 'fastify'
 const fastify = Fastify({
   logger: true
 })
 
-const apiKey = '6caafe474bb8d2535f39786ffbbd975b'
-
-fastify.get('/getWeather/:city', async (request, reply) => {
-    const urlcity = `http://api.openweathermap.org/geo/1.0/direct?q=${request.params.city}&limit=1&appid=${apiKey}`
-    const city = await Got(urlcity)
-    const parsed_city = JSON.parse(city.body)
-    const lat = parsed_city[0].lat
-    const long = parsed_city[0].lon
-
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&exclude=hourly,daily,minutely,alerts&appid=${apiKey}`
-    const weather = await Got(url)
-    const parsed_body = JSON.parse(weather.body)
-    const city_weather = parsed_body.current.temp
-    //const texto = 'La temperatura en Rio Cuarto es ' + (city_weather > 15 ? 'MAYOR ' : 'MENOR ') + 'A 15 GRADOS CELCIUS'
-
-    reply.send({ isHigherThan15: city_weather > 15 })
-})
-
-//fastify.register(weather)
+fastify.register(weather)
 
 fastify.listen(3000, function (err, address) {
   if (err) {
